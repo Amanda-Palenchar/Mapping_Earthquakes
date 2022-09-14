91085175ad7d4af74cb30a41adb9a643950979f1
Amanda Palenchar <amanda.palenchar@gmail.com>
2022-09-14 12:11:10 -0600

    Updating API key.

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -1,8 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-let API_KEY = "pk.eyJ1IjoiYXBhbGVuY2hhciIsImEiOiJjbDdxaWxjOXUwNW0wM3Rta3F4MDRreWdoIn0.IxqHdY76j63LumJknaVOyw";
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
